2e62326651904a9203d209334e72efaf2a72ac4c
Rodrigo <rodrigo@rodrigo-VirtualBox.skynet>
2016-02-07 12:28:16 -0200

    Builded basic version of login and signup system

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,15 +3,59 @@
 var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+  var clickHandler = new ClickHandler(db);
 
-   app.route('/')
-      .get(function (req, res) {
-         res.sendFile(process.cwd() + '/public/index.html');
+  app.route('/')
+    .get(function (req, res) {
+        res.sendFile(process.cwd() + '/public/index.html');
+    });
+
+  app.route('/signup')
+    .get(function (req, res) {      
+      res.render('signup')})
+    .post(function (req, res){
+      var name = req.body.name;
+      var password = req.body.password;
+
+      var users = db.collection('users');
+
+      var userInsertObject = {'name' : name, 'password' : password}
+
+      users.insert(userInsertObject, function (err) {
+         if (err) {
+            throw err;
+            res.end('Erro ao cadastrar')
+         }
+
+         res.end('Cadastrado com sucesso')
+      });      
+    });
+
+  app.route('/login')
+    .get(function (req, res){
+      res.render('login');
+    })
+    .post(function(req, res){
+      var name = req.body.name;
+      var password = req.body.password;
+
+      var users = db.collection('users');
+
+      users.findOne({"name" : name, "password" : password}, {}, function (err, doc){
+        if (err)
+          throw err;
+
+        if (doc == null)
+          res.end('Login incorreto')
+        else
+          res.redirect('/pools')
+
+        res.end(JSON.stringify(doc));
       });
+    });
 
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+  app.route('pools')
+    .get(function (req, res){
+      res.render('pools')
+    })
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
+var bodyParser = require('body-parser')
 
 var app = express();
 
@@ -16,11 +17,15 @@ mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
    app.use('/public', express.static(process.cwd() + '/public'));
    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-
+   app.set('view engine', 'jade');
+   app.use( bodyParser.json() );       // to support JSON-encoded bodies
+   app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+     extended: true
+   }));
+   
    routes(app, db);
 
    app.listen(3000, function () {
       console.log('Node.js listening on port 3000...');
    });
-
 });
